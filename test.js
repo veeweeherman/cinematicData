630dc0e601975d862484a359f9a8c506893591c0
veeweeherman <vy.hoang.cu@gmail.com>
2015-11-30 22:49:04 -0800

    jquery functions to call dat afrom json files

---
+++ test.js
@@ -0,0 +1,38 @@
+var jqxhr = $.getJSON( "actors.json", function(data) {
+  console.log( "success",data);
+})
+  .done(function(data) {
+    console.log( "second success",data );
+  })
+  .fail(function(data) {
+    console.log( "error" ,data);
+  })
+  .always(function(data) {
+    console.log( "complete" ,data);
+  });
+
+var jqxhr2 = $.getJSON( "directors.json", function(data) {
+  console.log( "success",data);
+})
+  .done(function(data) {
+    console.log( "second success",data );
+  })
+  .fail(function(data) {
+    console.log( "error" ,data);
+  })
+  .always(function(data) {
+    console.log( "complete" ,data);
+  });
+
+// $.getJSON( "/actors.json", function( data ) {
+//   console.log('djcskldnklsndknskdnk',data)
+//   var items = [];
+//   $.each( data, function( key, val ) {
+//     items.push( "<li id='" + key + "'>" + val + "</li>" );
+//   });
+ 
+//   $( "<ul/>", {
+//     "class": "my-new-list",
+//     html: items.join( "" )
+//   }).appendTo( "body" );
+// });
