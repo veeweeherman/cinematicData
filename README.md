24d46dcd38c9cf4fa1d8006efb4a840f59bc19f1
veeweeherman <vy.hoang.cu@gmail.com>
2015-12-06 14:28:19 -0800

    title shows in tabs now; title={} needs to go in the child component rendering of the parent

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ explanation of technologies
 
 browserify -t [ babelify --prets [ react ] ]  src/*.js -o bundle.js
 
+helpful tips/thanks to:
+https://github.com/pedronauck/react-simpletabs
+http://tylermcginnis.com/reactjs-tutorial-pt-2-building-react-applications-with-gulp-and-browserify/
 
 clone
 npm install
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,124 @@
+'use strict';
+
 var React = require('react');
 var ReactDOM = require('react-dom');
-// var actors = require('./data.js');
-// console.log('actors??',actors);
+var Tabs = require('react-simpletabs');
+
+var actors = require('./data.js');
+console.log('actors??',actors);
+//
+//
+// var data = [
+//   { title: "Actors", content: "I am the content of the first tab." },
+//   { title: "Movies", content: "I am the content of the second tab." },
+//   { title: "Directors", content: "Third tab, buddy." }
+// ];
+//
+// var Tabs = React.createClass({
+//   getInitialState: function() {
+//     return {activeTab: 0};
+//   },
+//   handleClick: function(index) {
+//     this.setState({activeTab: index});
+//     return false;
+//   },
+//   render: function() {
+//     return (
+//       <div>
+//         <dl className="tabs">
+//           {this.props.data.map(function (tab, index) {
+//             var activeClass = this.state.activeTab === index ? 'active' : '';
+//
+//             return (
+//               <dd className={'tab ' + activeClass} >
+//                 <a href="#" onClick={this.handleClick.bind(this, index)}>{tab.title}</a>
+//               </dd>
+//             )
+//           }, this)}
+//         </dl>
+//         <div className="tabs-content">
+//           {this.props.data.map(function (tab, index) {
+//             var activeClass = this.state.activeTab === index ? 'active' : '';
+//
+//             return (
+//               <div className={'content ' + activeClass}>
+//                 <p>{tab.content}</p>
+//               </div>
+//             )
+//           }, this)}
+//         </div>
+//       </div>
+//     );
+//   }
+// });
+//
+// ReactDOM.render(
+//   <Tabs data={data} />,
+//   document.getElementById('tabs')
+// );
+
+
+
+
+
+var App = React.createClass({
+  getInitialState: function(){
+    return {
+      categories: ['Actors', 'Movies', 'Directors'],
+    }
+  },
+  onMount: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
+    console.log('component DUD mount, showing tab ' + selectedIndex);
+  },
+  onBeforeChange: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
+    console.log('before the tab ' + selectedIndex);
+  },
+  onAfterChange: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
+    console.log('after the tab ' + selectedIndex);
+  },
+  render: function() {
+    return (
+      <Tabs tabActive={1} onBeforeChange={this.onBeforeChange} onAfterChange={this.onAfterChange} onMount={this.onMount}>
+
+          {this.state.categories.map(function(category,i){
+            return <CategoriesList key={i} categories={category} title={category}/>;
+          })}
+
+      </Tabs>
+    );
+  }
+});
+        // <Tabs.Panel title='Actors'>
+        //   <h2>list of actors</h2>
+        // </Tabs.Panel>
+        // <Tabs.Panel title='Movies'>
+        //   <h2>list of movies</h2>
+        // </Tabs.Panel>
+        // <Tabs.Panel title='Directors'>
+        //   <h2>list of directors</h2>
+        // </Tabs.Panel>
+
+var CategoriesList = React.createClass({
+  render: function(){
+    console.log('hello???', this.props.categories);
+      return (
+
+        <Tabs.Panel title={this.props.categories}>
+          <h2>list list list {this.props.categories}</h2>
+        </Tabs.Panel>
+      );
+  }
+
+});
+
+ReactDOM.render(<App />, document.getElementById('tabs'));
+
+
+
+
+
+
+
 //
 // var Categories = React.createClass({
 //   getInitialState: function(){
@@ -34,36 +151,3 @@ var ReactDOM = require('react-dom');
 //
 //
 // ReactDOM.render(<Categories />,document.getElementById('app'));
-
-
-'use strict';
-
-var Tabs = require('react-simpletabs');
-var App = React.createClass({
-  onMount: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
-    console.log('on mount, showing tab ' + selectedIndex);
-  },
-  onBeforeChange: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
-    console.log('before the tab ' + selectedIndex);
-  },
-  onAfterChange: function(selectedIndex, $selectedPanel, $selectedTabMenu) {
-    console.log('after the tab ' + selectedIndex);
-  },
-  render: function() {
-    return (
-      <Tabs tabActive={2} onBeforeChange={this.onBeforeChange} onAfterChange={this.onAfterChange} onMount={this.onMount}>
-        <Tabs.Panel title='Tab #1'>
-          <h2>Content #1</h2>
-        </Tabs.Panel>
-        <Tabs.Panel title='Tab #2'>
-          <h2>Content #2</h2>
-        </Tabs.Panel>
-        <Tabs.Panel title='Tab #3'>
-          <h2>Content #3</h2>
-        </Tabs.Panel>
-      </Tabs>
-    );
-  }
-});
-
-ReactDOM.render(<App />, document.getElementById('app'));
